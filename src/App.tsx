a3e2e17bb6a4e997176c62cf2713d9c89d7308bd
ankitrana82 <ankit0175.becse24@chitkara.edu.in>
2025-04-12 00:14:48 +0530

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <h1>Welcome to BudgetBuddy</h1>
+      <Footer/>
+      
     </div>
   );
 }
